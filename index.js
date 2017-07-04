e688f5c41def4f59d51fdd9694f27da7af1e8764
wethinkdigital <david@dlrobins.co.uk>
2017-07-04 11:00:23 +0100

    router update

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 const express = require('express');
 var app = new express();
 
+app.get('/', (req,res) => {
+	res.send('index page');
+});
+
 app.get('/test', (req,res) => {
 	res.send('testing');
 });
